c2eac887e605aace9b2d86f5e0cc082d11dd208b
Umesh Kumar <umeshighe@gmail.com>
2022-10-09 22:49:58 +0530

    add status code

--- src/main/java/playground/training/GetMetaData.java
+++ src/main/java/playground/training/GetMetaData.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
-//java -cp playground-0.0.1-SNAPSHOT.jar playground.training.GetMetaData
+
+//java -cp playground-0.0.1-SNAPSHOT-jar-with-dependencies.jar playground.training.GetMetaData
 public class GetMetaData {
 
 	public static void main(String[] args) throws IOException {
@@ -13,8 +14,8 @@ public class GetMetaData {
 		Request request = new Request.Builder().url("http://metadata.google.internal/computeMetadata/v1/instance/image")
 				.method("GET", null).addHeader("Metadata-Flavor", "Google").build();
 		Response response = client.newCall(request).execute();
-	int a =	response.code();
-	System.out.println("Response Code is this");
+		int a = response.code();
+		System.out.println("Response Code is this" + a);
 
 	}
 
