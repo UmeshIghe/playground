553a3dbc58c04ae2e4c2d3775643cec41d252ccc
Umesh Kumar <umeshighe@gmail.com>
2022-10-10 14:18:42 +0530

    add junit

---
+++ src/test/java/playground/JsonParserTest.java
@@ -0,0 +1,18 @@
+package playground;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class JsonParserTest {
+
+	@Test
+	public void testJson() {
+		System.out.println("Jai mata di");
+
+		assertEquals("Umesh", "Umesh");
+	}
+
+}
